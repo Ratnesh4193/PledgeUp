85ca2589de7998206d4489540405dfcb10091b0a
Ratnesh <rktssgk@gmail.com>
2023-10-30 23:19:21 +0530

    Third7 Deployment

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -4,10 +4,12 @@ if (window.web3 !== undefined) {
   console.log("MetaMask Account present");
   web3 = new Web3(window.web3.currentProvider);
 } else {
-  console.log("No MetaMask Account present. Please install metamask.");
   const provider = new Web3.providers.HttpProvider(
     process.env.REACT_APP_SEPOLIA
   );
   web3 = new Web3(provider);
+  console.log("No MetaMask Account present. Please install metamask.");
+  console.log(process.env.REACT_APP_SEPOLIA);
+  console.log(web3);
 }
 export default web3;
